105b70c888e2dc5098f7e57771c657c44a073b17
schandaluri <schandaluri@nisum.com>
2021-06-23 12:25:52 -0700

    Initial commit

---
+++ src/test/java/com/example/demo/MyStepdefs.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import java.util.ArrayList;
+import static io.restassured.RestAssured.*;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class MyStepdefs {
+
+
+}
